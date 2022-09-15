3cde440df2b48f99028d8060787b6c76f67ed23a
Justin Webb <Webb.n.justin@gmail.com>
2022-09-15 14:03:08 -0400

    Started on a carosel

--- tin-dogz/src/Components/Home Things/HomeCarosel.js
+++ tin-dogz/src/Components/Home Things/HomeCarosel.js
@@ -1,8 +1,15 @@
+
 function HomeCarosel(props) {
     
     return (
         <div class="slideshow-container">
-
+            <p>Hello</p>
+            <div className="mySlides fade">
+                
+                <div className="numbertext"> 1/3 </div>
+                {/* <img src="lady" style="width:100%"/> */}
+                <div className="test"></div>
+            </div>
         </div>
     )
 }
