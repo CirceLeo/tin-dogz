d6bed9d6061e2e8158633698dde212b764a98b87
Justin Webb <Webb.n.justin@gmail.com>
2022-09-22 12:28:47 -0400

    Created MVP pages and confirmed file structure

--- tin-dogz/src/App.js
+++ tin-dogz/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import NavBar from './Components/NavBar';
+import NavBar from './Components/Universals/NavBar';
 import HomePage from './Components/Home Things/HomePage';
+import {Potential, Profile} from './Components/Users'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+     
+      <Potential/>
+      <Profile/>
       <HomePage />
     </div>
   );
---
+++ tin-dogz/src/Components/Users/Potential.js
@@ -0,0 +1,6 @@
+
+function Potential(){
+
+}
+
+export default Potential;
