5bbdf9b7f123b2c8c6e3f2198ac3124ba29f1807
Calypso Leonard <calypsorleonard@gmail.com>
2022-09-22 12:12:52 -0400

    moving things

--- tin-dogz/src/Components/Home Things/HomeCarosel.js
+++ tin-dogz/src/Components/Home Things/HomeCarosel.js
@@ -1,13 +1,14 @@
-
+import lady from "./Photos/lady.png"
 function HomeCarosel(props) {
     
     return (
         <div class="slideshow-container">
             <p>Hello</p>
+                <img src={lady} alt="lady and tramp"/>
             <div className="mySlides fade">
+                <p>tramp</p>
                 
-                <div className="numbertext"> 1/3 </div>
-                {/* <img src="lady" style="width:100%"/> */}
+                <div className="numbertext"> 1/3</div>
                 <div className="test"></div>
             </div>
         </div>
--- tin-dogz/src/Components/NavBar.js
+++ tin-dogz/src/Components/NavBar.js
@@ -6,16 +6,16 @@ function NavBar(props) {
             <div class="container-fluid">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link">I</a>
+                        <a href={"home"} class="nav-link">I</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link">Love</a>
+                        <a href={"home"} class="nav-link">Love</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link">Dogs</a>
+                        <a href={"home"} class="nav-link">Dogs</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link">Login</a>
+                        <a href={"home"} class="nav-link">Login</a>
                     </li>
                 </ul>
             </div>
